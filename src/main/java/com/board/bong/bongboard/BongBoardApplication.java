75a5821102f0e2a3037414ea67656866eab30fc9
khb6997@gmail.com <khb6997@gmail.com>
2023-01-24 00:23:08 +0900

    start springboot project

---
+++ src/main/java/com/board/bong/bongboard/BongBoardApplication.java
@@ -0,0 +1,13 @@
+package com.board.bong.bongboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BongBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BongBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/board/bong/bongboard/BongBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.board.bong.bongboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BongBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
